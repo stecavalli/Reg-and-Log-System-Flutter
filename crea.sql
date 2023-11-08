f9948809b2636ba399f1b1db2e79d74c739fb09e
Stefano Cavalli <stefanocavalli@outlook.com>
2023-11-08 13:04:12 +0100

    Update crea.sql

--- crea.sql
+++ crea.sql
@@ -4,12 +4,12 @@ CREATE TABLE users (
     lastname varchar(45) NOT NULL,
     mail varchar(45) NOT NULL,
     username varchar(45) NOT NULL,
-	password varchar(255) NOT NULL,
-	active char(1) NOT NULL DEFAULT '0'
+    password varchar(255) NOT NULL,
+    active char(1) NOT NULL DEFAULT '0'
 );
 
 CREATE TABLE users_activation (
     username varchar(45) PRIMARY KEY NOT NULL,
-	token varchar(255) NOT NULL,
-	created_at Date NOT NULL
+    token varchar(255) NOT NULL,
+    created_at Date NOT NULL
 );
